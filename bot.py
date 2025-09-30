195c1087586f8fce9f7a686ed0d892d916166250
AngelRomero23 <angelromeroag@hotmail.com>
2025-09-29 20:36:52 -0500

    tests

--- bot.py
+++ bot.py
@@ -1,24 +1,31 @@
+# bot.py
+import logging
 import discord
 from discord.ext.commands import Bot
 from settings.settings import DISCORD_TOKEN
 from events.on_message import manejar_mensaje
 from events.slash_commands import setup_slash_commands
 
+# Configurar logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = Bot(command_prefix="!", intents=intents)
+bot: Bot = Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready() -> None:
-    print(f"Bot conectado como {bot.user}")
+    logging.info(f"Bot conectado como {bot.user}")
     await setup_slash_commands(bot)
-    await bot.tree.sync()  # Sincroniza los slash commands en Discord
-    print("Slash commands sincronizados")
+    await bot.tree.sync()
+    logging.info("Slash commands sincronizados correctamente")
 
 @bot.event
 async def on_message(message: discord.Message) -> None:
-    print(f"Mensaje recibido en {message.channel.id} de {message.author}: {message.content}")
     await manejar_mensaje(message, bot)
 
 bot.run(DISCORD_TOKEN)
